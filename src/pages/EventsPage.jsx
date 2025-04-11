ce4db8c7c7ffb04644963dc8ed29e1af24e988c0
ReverendTrivium <popetrivium@gmail.com>
2025-04-11 16:17:07 -0400

    Modified Events Page to have Layout match Rest of the Website. Left Placeholder texted for now before I link the Discord Bot to the Webpage to grab Events.

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -4,8 +4,13 @@ import '../styles/EventsPage.css';
 const EventsPage = () => {
   return (
     <div className="events-page">
-      <h1>Upcoming Events</h1>
-      <p>Stay tuned for exciting upcoming events!</p>
+      <div className="events-header">
+        <h1 className="text-white text-uppercase">Upcoming Events</h1>
+        <div className="logo-container">
+          <img src="/img/logo.png" alt="Tomodachi Street Logo" className="events-logo" />
+        </div>
+      </div>
+      <p className="event-description text-white">Stay tuned for exciting upcoming events!</p>
     </div>
   );
 };
